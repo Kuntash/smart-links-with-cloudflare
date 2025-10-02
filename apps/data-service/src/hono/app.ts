ae1be20eb68acbe219af32548b00a8fc31e483e8
Kunga Tashi <kuntashtashi11@gmail.com>
2025-10-02 23:19:49 +0530

    Hono setup, destination logic and db setup

---
+++ apps/data-service/src/hono/app.ts
@@ -0,0 +1,28 @@
+import { getDestinationForCountry } from '@/helpers/route-ops';
+import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { Hono } from 'hono';
+
+
+export const app = new Hono<{ Bindings: Env }>();
+
+app.get('/:id', async (c) => {
+  const id = c.req.param('id');
+
+  const linkInfoFromDB = await getLink(id);
+
+  if (!linkInfoFromDB) {
+    return c.json({ error: 'Destination not found' }, 404);
+  }
+
+  const cfHeaders = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+  if (!cfHeaders.success) {
+    return c.text('Invalid Cloudflare headers', 400);
+  }
+
+  const headers = cfHeaders.data;
+  const destination = getDestinationForCountry(linkInfoFromDB, headers.country);
+  return c.redirect(destination);
+})
+
+export default app;
