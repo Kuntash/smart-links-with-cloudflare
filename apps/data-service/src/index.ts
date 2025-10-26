e94d93def375c4be71c50cd2266b384c58b125ed
Kunga Tashi <kuntashtashi11@gmail.com>
2025-10-26 23:36:37 +0530

    Add link click handling to queue processing
    - Updated queue processing to parse and handle LINK_CLICK events.
    - Introduced a new handler for link clicks in `queue-handlers/link-clicks.ts`.
    - Added `addLinkClick` function to insert link click data into the database.
    - Configured the queue producer to include a remote flag in `wrangler.jsonc`.

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,6 +1,8 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import app from './hono/app';
 import { initDatabase } from '@repo/data-ops/database';
+import { QueueMessageSchema } from '@repo/data-ops/zod-schema/queue';
+import { handleLinkClick } from './queue-handlers/link-clicks';
 
 export default class DataService extends WorkerEntrypoint<Env> {
 
@@ -13,7 +15,25 @@ export default class DataService extends WorkerEntrypoint<Env> {
 	}
 	async queue(batch: MessageBatch<unknown>) {
 		for (const message of batch.messages) {
-			console.log("Queue event:", message.body);
+			const parsedEvent = QueueMessageSchema.safeParse(message.body);
+
+			if (!parsedEvent.success) {
+				console.error(parsedEvent.error);
+				return;
+			}
+
+			const event = parsedEvent.data;
+
+			switch (event.type) {
+				case "LINK_CLICK": {
+					await handleLinkClick(this.env, event);
+					break;
+				}
+				default: {
+					console.error(`Unknown event type: ${event.type}`);
+					return;
+				}
+			}
 		}
 	}
 }
